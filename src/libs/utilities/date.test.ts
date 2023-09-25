80db3e633326fa7c3799446f912333582ebd2e29
Soheil Movahedan <soheil.movahhedan@gmail.com>
2023-09-25 17:11:02 +0200

    Remove dayjs and edit utilities/date

--- src/libs/utilities/date.test.ts
+++
@@ -1,8 +0,0 @@
-import { formatDate } from "./date";
-
-describe("date utility", () => {
-  test("formatDate", () => {
-    const formattedDate = formatDate(new Date("1/1/2023"));
-    expect(formattedDate).toBe("2023-01-01");
-  });
-});
--- src/libs/utilities/date.ts
+++ src/libs/utilities/date.ts
@@ -1,4 +1,4 @@
-import dayjs from "dayjs";
+export const DATE_FORMAT = "YYYY-MM-DD";
 
-export const dateFormat = "YYYY-MM-DD";
-export const formatDate = (date: Date) => dayjs(date).format(dateFormat);
+export const WEEKS_IN_MILLISECONDS = 7 * 24 * 60 * 60 * 1000;
+export const WEEKS_PER_YEAR = Math.floor(365 / 7);
