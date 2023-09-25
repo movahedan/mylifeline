4224ab3bf71ba104569700908acc95422747e636
Soheil Movahedan <soheil.movahhedan@gmail.com>
2023-09-25 16:54:22 +0200

    Edit utilities/string to support more types

--- src/libs/utilities/string.ts
+++ src/libs/utilities/string.ts
@@ -1,6 +1,12 @@
 export const classNames = (
-  classNamesArray: (string | undefined)[] | string | undefined
+  classNamesArray:
+    | (string | undefined | boolean)[]
+    | string
+    | undefined
+    | boolean
 ) =>
   typeof classNamesArray === "string" || classNamesArray === undefined
     ? classNamesArray
+    : typeof classNamesArray === "boolean"
+    ? undefined
     : classNamesArray.filter(Boolean).join(" ");
