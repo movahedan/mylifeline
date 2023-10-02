22ca5034fc8ec8453b3de47d29aa8048b2876d25
Soheil Movahedan <soheil.movahhedan@gmail.com>
2023-10-02 17:04:49 +0200

    ci(linters): Add commitlint, branchlint, and standard-version to the repository.
    You can now commit using `yarn commit` and use its CLI to commit following the commit message
    convention. Plus, your branch name will get checked when you want to push your changes to the
    origin. Use `yarn release` to do a release and update the CHANGELOG.md automatically. Have fun
    coding!

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  "**/*.ts?(x)": (files) => [
-    "yarn type-check",
-    `yarn lint --quiet --fix`,
-    `yarn test --bail --findRelatedTests ${files.join(" ")}`,
-  ],
+  "**/*.ts?(x)": (files) => {
+    const scripts = ["yarn type-check", `yarn lint --quiet --fix`];
+
+    const filesString = files.join(" ");
+    const testScript = `yarn test --bail --passWithNoTests --findRelatedTests ${filesString}`;
+    if (files.length) scripts.push(testScript);
+
+    return scripts;
+  },
 };
